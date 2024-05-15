b991c941a2ce3c48f4a3aa88eb2a8fcb648825fd
salihgueler <muhammedsalihguler@gmail.com>
2024-05-15 19:12:28 +0300

    Remove all to use something funny.

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,41 @@
-import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
-import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const sub = client.subscriptions.receive().subscribe({
+    next: (data) => {
+      console.log(data);
+    },
+    error: (err) => {
+      console.error(err);
+    },
+    complete: () => {
+      console.log("complete");
+    },
+  });
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+  function unsub() {
+    sub.unsubscribe();
   }
 
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id });
+  function addItem() {
+    client.mutations.publish({
+      channelName: "world",
+      content: "My first message!",
+    });
   }
 
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>My todos</h1>
-          <button onClick={createTodo}>+ new</button>
-          <h1>{user?.signInDetails?.loginId}'s todos</h1>
-          <ul>
-            {todos.map((todo) => (
-              <li onClick={() => deleteTodo(todo.id)} key={todo.id}>
-                {todo.content}
-              </li>
-            ))}
-          </ul>
-          <div>
-            🥳 App successfully hosted. Try creating a new todo.
-            <br />
-            <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-              Review next step of this tutorial.
-            </a>
-          </div>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+    <main>
+      <h1>My Custom Subs</h1>
+      <button onClick={unsub}>Unsubscribe</button>
+      <h1></h1>
+      <button onClick={addItem}>Add Item</button>
+    </main>
   );
 }
 
